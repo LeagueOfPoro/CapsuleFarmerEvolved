d604fdbabf8a5197022333300351cfc52b4b3c5c
François Darveau <franklebest07@gmail.com>
2022-11-04 22:54:17 -0400

    add dockerfile and support getting config from environment variables if defined

--- Config.py
+++ Config.py
@@ -1,12 +1,13 @@
 import yaml
+import os
 
 class Config:
     def __init__(self) -> None:
         with open("config.yaml", "r",  encoding='utf-8') as f:
             config = yaml.safe_load(f)
-            self.username = config["username"]
-            self.password = config["password"]
-            self.debug = config.get("debug", False)
+            self.username = os.environ.get('RIOT_USERNAME')
+            self.password = os.environ.get('RIOT_PASSWORD')
+            self.debug = os.environ.get('DEBUG', config.get("debug", False))
 
         with open("bestStreams.txt", "r",  encoding='utf-8') as f:
             self.bestStreams = f.read().splitlines()
