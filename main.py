cb8e85de3af84b6dc0c6693cdb03a5ee2302c850
League of Poro <95635582+LeagueOfPoro@users.noreply.github.com>
2023-01-19 10:22:23 +0100

    Version bump

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from rich import print
 from Stats import Stats
 from VersionManager import VersionManager
 
-CURRENT_VERSION = 0.3
+CURRENT_VERSION = 0.4
 
 parser = argparse.ArgumentParser(description='Farm Esports Capsules by watching all matches on lolesports.com.')
 parser.add_argument('-c', '--config', dest="configPath", default="./config.yaml",
