7c926d2e3e8152ec2955a3620a4276869492d4a9
League of Poro <95635582+LeagueOfPoro@users.noreply.github.com>
2023-01-31 12:57:57 +0100

    Show number of drops in GUI

--- GuiThread.py
+++ GuiThread.py
@@ -28,13 +28,13 @@ class GuiThread(Thread):
         table.add_column("Status")
         table.add_column("Live matches")
         table.add_column("Last check")
-        # table.add_column("Last drop")
-        # table.add_column("Drops")
+        table.add_column("Last drop")
+        table.add_column("Drops")
 
         for acc in self.stats.accountData:
             status = self.stats.accountData[acc]["status"]
-            # table.add_row(f"{acc}", f"{status}", f"{self.stats.accountData[acc]['liveMatches']}", f"{self.stats.accountData[acc]['lastCheck']}", f"{self.stats.accountData[acc]['lastDrop']}", f"{self.stats.accountData[acc]['totalDrops']}")
-            table.add_row(f"{acc}", f"{status}", f"{self.stats.accountData[acc]['liveMatches']}", f"{self.stats.accountData[acc]['lastCheck']}")
+            table.add_row(f"{acc}", f"{status}", f"{self.stats.accountData[acc]['liveMatches']}", f"{self.stats.accountData[acc]['lastCheck']}", f"{self.stats.accountData[acc]['lastDrop']}", f"{self.stats.accountData[acc]['totalDrops']}")
+            # table.add_row(f"{acc}", f"{status}", f"{self.stats.accountData[acc]['liveMatches']}", f"{self.stats.accountData[acc]['lastCheck']}")
         return table
 
     def run(self):
