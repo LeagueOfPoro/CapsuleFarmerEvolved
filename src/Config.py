a884644cb7ac812c53e4f1eb29f02f2bb9da74b7
Nicklas <nicklas.henri@gmail.com>
2023-02-08 01:12:56 +0100

    Fixed spacing

--- src/Config.py
+++ src/Config.py
@@ -40,20 +40,17 @@ class Config:
                 self.debug = config.get("debug", False)
                 self.connectorDrops = config.get("connectorDropsUrl", "")
         except FileNotFoundError as ex:
-            print(
-                f"[red]CRITICAL ERROR: The configuration file cannot be found at {configPath}\nHave you extacted the ZIP archive and edited the configuration file?")
+            print(f"[red]CRITICAL ERROR: The configuration file cannot be found at {configPath}\nHave you extacted the ZIP archive and edited the configuration file?")
             print("Press any key to exit...")
             input()
             raise ex
         except (ParserError, KeyError) as ex:
-            print(
-                f"[red]CRITICAL ERROR: The configuration file does not have a valid format.\nPlease, check it for extra spaces and other characters.\nAlternatively, use confighelper.html to generate a new one.")
+            print(f"[red]CRITICAL ERROR: The configuration file does not have a valid format.\nPlease, check it for extra spaces and other characters.\nAlternatively, use confighelper.html to generate a new one.")
             print("Press any key to exit...")
             input()
             raise ex
         except InvalidCredentialsException as ex:
-            print(
-                f"[red]CRITICAL ERROR: There are only default credentials in the configuration file.\nYou need to add you Riot account login to config.yaml to receive drops.")
+            print(f"[red]CRITICAL ERROR: There are only default credentials in the configuration file.\nYou need to add you Riot account login to config.yaml to receive drops.")
             print("Press any key to exit...")
             input()
             raise ex
@@ -69,8 +66,7 @@ class Config:
                 pass
         # Fallback to local file
         if self.bestStreams is None:
-            print(
-                f"[yellow]WARNING: The bestStreamsUrl is not available. Are you sure it's a accessible to the public? Using local file instead.")
+            print(f"[yellow]WARNING: The bestStreamsUrl is not available. Are you sure it's a accessible to the public? Using local file instead.")
             try:
                 bestStreams = Path("bestStreams.txt")
                 if Path("../config/bestStreams.txt").exists():
@@ -109,5 +105,4 @@ class Config:
             return Path("../config/config.yaml")
         if Path("config/config.yaml").exists():
             return Path("config/config.yaml")
-
         return configPath
