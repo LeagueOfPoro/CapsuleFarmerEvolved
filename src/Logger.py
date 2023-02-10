97a86823ff8a41dad845d6326a50fa67f9941a1f
Sizz <71391035+s-Sizz@users.noreply.github.com>
2023-02-10 10:27:03 +0200

    feat: change log handler to RotatingFileHandler (#111)
    * change logger to `RotatingFIleHandler`
    
    * changed log filename

--- src/Logger.py
+++ src/Logger.py
@@ -1,6 +1,8 @@
 import logging
-import logging.config
-from datetime import datetime
+from logging.handlers import RotatingFileHandler
+
+FILE_SIZE = 1024 * 1024 * 100  # 100 MB
+BACKUP_COUNT = 5  # keep up to 5 files
 
 
 class Logger:
@@ -11,10 +13,18 @@ class Logger:
         else:
             level = logging.WARNING
 
-        logging.basicConfig(filename=f'./logs/capsulefarmer-{datetime.now().strftime("%Y-%m-%d")}.log',
-                            filemode="a+",
-                            format='%(asctime)s %(levelname)s: %(message)s',
-                            level=level)
+        fileHandler = RotatingFileHandler(
+            "./logs/capsulefarmer.log",
+            mode="a+",
+            maxBytes=FILE_SIZE,
+            backupCount=BACKUP_COUNT,
+        )
+
+        logging.basicConfig(
+            format="%(asctime)s %(levelname)s: %(message)s",
+            level=level,
+            handlers=[fileHandler],
+        )
         log = logging.getLogger("League of Poro")
         log.info("-------------------------------------------------")
         log.info("---------------- Program started ----------------")
